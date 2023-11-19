bb94eae9d1f77576a99b7051d8555a64a02ccbc5
Ashwani Singh <rrvapmr@gmail.com>
2023-11-19 22:11:08 +0530

    added some models aysncHandler

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 //require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-dotenv.config({
-  path: "./env",
-});
+dotenv.config(); //{path: "./env"}
+import { app } from "./app.js";
 
-connectDB();
+const port = process.env.PORT;
+
+connectDB()
+  .then(() => {
+    app.listen(port || 8000, () => {
+      console.log(`server is running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("mongo db connection failed");
+  });
